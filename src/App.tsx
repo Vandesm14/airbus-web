96ac4731f8754077e0f397acb8a668c5076b075d
Shane <vandesm14@gmail.com>
2025-02-09 16:10:59 -0500

    split APU into a panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import Button from './components/Button';
-import Group from './components/Group';
-import Knob from './components/Knob';
-import WithLabel from './components/WithLabel';
+import Group from '~/components/Group';
 
-import { createEffect, createMemo, createSignal, onMount } from 'solid-js';
+import APU from './panels/APU';
 
 export default function App() {
-  const [master, setMaster] = createSignal(false);
-  const [start, setStart] = createSignal(false);
-  const [apuSpeed, setApuSpeed] = createSignal(0);
-
-  createEffect(() => {
-    if (!master()) {
-      setStart(false);
-    }
-  });
-
-  onMount(() => {
-    const interval = setInterval(() => {
-      if (start()) {
-        setApuSpeed((x) => Math.min(x + 1, 100));
-      } else {
-        setApuSpeed((x) => Math.max(x - 1, 0));
-      }
-    }, 100);
-
-    return () => clearInterval(interval);
-  });
-
-  const apuAvail = createMemo(() => apuSpeed() === 100);
-
   return (
     <>
-      <span>APU Speed: {apuSpeed()}</span>
-      <Group direction="column">
-        <WithLabel label="MASTER">
-          <Button kind="on" on={master()} setOn={setMaster} />
-        </WithLabel>
-        <WithLabel label="START">
-          <Button
-            kind="on"
-            on={start()}
-            setOn={(x) => master() && setStart(x)}
-            avail={apuAvail()}
-          />
-        </WithLabel>
-        <Knob
-          detents={[
-            { label: 'ISOLATE', angle: 45 },
-            { label: 'STARTUP', angle: 90 },
-            { label: 'SUPPLY', angle: 135 },
-          ]}
-          default={apuSpeed()}
-        />
+      <Group direction="row">
+        <APU />
       </Group>
     </>
   );
