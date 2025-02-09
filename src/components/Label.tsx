88a42c477a41f521b1a04be45530457e022c8e46
Shane <vandesm14@gmail.com>
2025-02-09 15:23:37 -0500

    allow multiple lines in label using \n

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,15 +1,15 @@
+import { createMemo, For } from 'solid-js';
 import './Label.scss';
 
-export default function Label({
-  text,
-  style,
-}: {
-  text: string;
-  style?: string;
-}) {
+export default function Label(props: { text: string; style?: string }) {
+  const text = () => props.text;
+  const style = () => props.style;
+
+  const split = createMemo(() => text().split('\n'));
+
   return (
-    <span class="label" style={style}>
-      {text}
+    <span class="label" style={style()}>
+      <For each={split()}>{(line) => <span>{line}</span>}</For>
     </span>
   );
 }
