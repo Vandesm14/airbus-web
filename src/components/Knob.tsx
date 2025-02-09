26a6f7bb81a79e63289087277366a8be0a93d693
Shane <vandesm14@gmail.com>
2025-02-09 14:41:35 -0500

    add comments and remove dead code

--- src/components/Knob.tsx
+++ src/components/Knob.tsx
@@ -1,7 +1,7 @@
 import './Knob.scss';
 import svg from './Knob.svg';
 
-import { createEffect, createMemo, createSignal, For } from 'solid-js';
+import { createMemo, createSignal, For } from 'solid-js';
 
 import Label from './Label';
 import { toRad } from '../lib/ang';
@@ -46,22 +46,21 @@ export default function Knob(props: KnobProps) {
     () => `transform: rotate(${detent()?.angle ?? 0}deg)`
   );
 
-  createEffect(() => {
-    console.log(bounds());
-  });
-
   const labels = createMemo(() => {
     let arr: Array<{ text: string; x: number; y: number }> = [];
     if (bounds()) {
       for (const detent of detents()) {
         const angle = toRad(detent.angle);
 
+        // Calculate the x and y coordinates of the label
         let x = Math.sin(angle) * 75;
         let y = Math.cos(angle) * 75;
 
+        // Offset based on the center of the knob
         x += bounds()!.left + bounds()!.width / 2;
         y += bounds()!.top + bounds()!.height / 2;
 
+        // Offset for font size (vertically)
         y -= 20;
 
         arr.push({ text: detent.label, x, y });
