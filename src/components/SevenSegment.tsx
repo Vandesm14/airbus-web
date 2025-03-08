b28d2ed8dffe407c9717a3cd0f68d248acda3cce
Shane <vandesm14@gmail.com>
2025-03-08 12:33:52 -0500

    support negatives and optional padding with zeros

--- src/components/SevenSegment.tsx
+++ src/components/SevenSegment.tsx
@@ -17,14 +17,20 @@ export function digitToSegment(digit: number): Segments {
     d: [0, 2, 3, 5, 6, 8, 9].includes(digit),
     e: [0, 2, 6, 8].includes(digit),
     f: [0, 4, 5, 6, 8, 9].includes(digit),
-    g: [2, 3, 4, 5, 6, 8, 9].includes(digit),
+    g: [-1, 2, 3, 4, 5, 6, 8, 9].includes(digit),
   };
 }
 
-export default function SevenSegment(props: { value: number }) {
+export default function SevenSegment(props: {
+  value: number;
+  digits?: number;
+  padWithZeros?: boolean;
+}) {
   let canvas!: HTMLCanvasElement;
 
   const value = () => props.value;
+  const digits = () => props.digits ?? 1;
+  const padWithZeros = () => props.padWithZeros ?? false;
 
   const size = 10;
   const ratio = 3;
@@ -45,61 +51,81 @@ export default function SevenSegment(props: { value: number }) {
     ctx.fillStyle = 'orange';
     ctx.strokeStyle = 'orange';
 
-    let digit = Number(value().toString().at(0)!);
-    let segments = digitToSegment(digit);
+    const string = value().toString();
+    const length = string.length;
 
-    if (segments.a) {
-      ctx.fillRect(padding + size, padding, size * ratio, size);
-    }
-    if (segments.b) {
-      ctx.fillRect(
-        padding + size + size * ratio,
-        padding + size,
-        size,
-        size * ratio
-      );
-    }
-    if (segments.c) {
-      ctx.fillRect(
-        padding + size + size * ratio,
-        padding + size * 2 + size * ratio,
-        size,
-        size * ratio
-      );
-    }
-    if (segments.d) {
-      ctx.fillRect(
-        padding + size,
-        padding + size * 2 + size * ratio * 2,
-        size * ratio,
-        size
-      );
-    }
-    if (segments.e) {
-      ctx.fillRect(
-        padding,
-        padding + size * 2 + size * ratio,
-        size,
-        size * ratio
-      );
-    }
-    if (segments.f) {
-      ctx.fillRect(padding, padding + size, size, size * ratio);
-    }
-    if (segments.g) {
-      ctx.fillRect(
-        padding + size,
-        padding + size + size * ratio,
-        size * ratio,
-        size
-      );
+    for (let i = 0; i < digits(); i++) {
+      const index = length - digits() + i;
+      const char = string[index];
+
+      let digit: number = 0;
+      if (char) {
+        if (char === '-') {
+          digit = -1;
+        } else {
+          digit = Number(char);
+        }
+      } else if (!padWithZeros()) {
+        continue;
+      }
+
+      let segments = digitToSegment(digit);
+
+      const offset = (size * 2 + size * ratio + padding * 2) * i;
+
+      if (segments.a) {
+        ctx.fillRect(offset + padding + size, padding, size * ratio, size);
+      }
+      if (segments.b) {
+        ctx.fillRect(
+          offset + padding + size + size * ratio,
+          padding + size,
+          size,
+          size * ratio
+        );
+      }
+      if (segments.c) {
+        ctx.fillRect(
+          offset + padding + size + size * ratio,
+          padding + size * 2 + size * ratio,
+          size,
+          size * ratio
+        );
+      }
+      if (segments.d) {
+        ctx.fillRect(
+          offset + padding + size,
+          padding + size * 2 + size * ratio * 2,
+          size * ratio,
+          size
+        );
+      }
+      if (segments.e) {
+        ctx.fillRect(
+          offset + padding,
+          padding + size * 2 + size * ratio,
+          size,
+          size * ratio
+        );
+      }
+      if (segments.f) {
+        ctx.fillRect(offset + padding, padding + size, size, size * ratio);
+      }
+      if (segments.g) {
+        ctx.fillRect(
+          offset + padding + size,
+          padding + size + size * ratio,
+          size * ratio,
+          size
+        );
+      }
     }
   });
 
   return (
     <canvas
       ref={canvas}
-      width={size * 2 + size * ratio + padding * 2}
+      width={(size * 2 + size * ratio + padding * 2) * digits()}
       height={size * 3 + size * ratio * 2 + padding * 2}
     ></canvas>
   );
