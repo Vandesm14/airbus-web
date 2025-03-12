367d7f2dbf6b488adc08c7b3cb4b92e251c9c8f2
Shane <vandesm14@gmail.com>
2025-03-12 12:09:30 -0400

    ignore target dir

--- target/debug/deps/engine-2cbe4704fc9d5c32.d
+++
@@ -1,9 +0,0 @@
-/home/shane/Desktop/projects/airbus-web/target/debug/deps/libengine-2cbe4704fc9d5c32.rmeta: engine/src/lib.rs Cargo.toml
-
-/home/shane/Desktop/projects/airbus-web/target/debug/deps/engine-2cbe4704fc9d5c32.d: engine/src/lib.rs Cargo.toml
-
-engine/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- target/debug/deps/engine-de4ee2b4745aa21c.d
+++
@@ -1,9 +0,0 @@
-/home/shane/Desktop/projects/airbus-web/target/debug/deps/libengine-de4ee2b4745aa21c.rmeta: engine/src/main.rs Cargo.toml
-
-/home/shane/Desktop/projects/airbus-web/target/debug/deps/engine-de4ee2b4745aa21c.d: engine/src/main.rs Cargo.toml
-
-engine/src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
