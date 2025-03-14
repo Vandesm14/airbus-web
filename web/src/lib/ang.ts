ebf101dfaf2cb679b707068884b40350a432687f
Shane <vandesm14@gmail.com>
2025-03-14 12:46:59 -0400

    use leptos

--- web/src/lib/ang.ts
+++
@@ -1,7 +0,0 @@
-export function toDeg(rad: number) {
-  return rad * (180 / Math.PI);
-}
-
-export function toRad(deg: number) {
-  return deg * (Math.PI / 180);
-}
--- web/src/lib/wasm.ts
+++
@@ -1,5 +0,0 @@
-import init, { greet } from '@engine/pkg/engine';
-
-init().then(() => {
-  greet('WebAssembly');
-});
