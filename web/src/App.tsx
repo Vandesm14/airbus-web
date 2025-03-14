b730d56f7a22fda7aa40afc77ae5b4456eaec5a5
Shane <vandesm14@gmail.com>
2025-03-14 12:38:47 -0400

    wip: game/engine

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import Group from './components/Group';
-import { createEngine, tick, toggleButton } from './engine';
-import Button from './components/Button';
-import { createEffect, createSignal, onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import WithLabel from './components/WithLabel';
 import SevenSegment from './components/SevenSegment';
 import Encoder from './components/Encoder';
 import { createAcceleration } from './hooks/speeder';
 
+import { Game } from '@engine/pkg/engine';
+
 export default function App() {
   const [lastTick, setLastTick] = createSignal(Date.now());
-  const [engine, setEngine] = createEngine();
   const speeder = createAcceleration();
+  const [game, setGame] = createSignal(new Game());
 
   const tickRate = 1000 / 30;
 
@@ -19,16 +19,16 @@ export default function App() {
       const now = Date.now();
       const dt = (now - lastTick()) / tickRate;
 
-      setEngine(tick(dt));
+      // setEngine(tick(dt));
 
       setLastTick(now);
     }, tickRate);
   });
 
   const onSetPointChange = (direction: number) => {
-    setEngine((e) => {
-      e.ng_set_point += speeder() * direction;
-      return e;
+    setGame((g) => {
+      g.set_control_rods_delta(speeder() * direction);
+      return g;
     });
   };
 
@@ -36,27 +36,19 @@ export default function App() {
     <>
       <Group direction="column">
         <Group direction="row" no_pad>
-          <WithLabel label="MASTER">
-            <Button
-              kind="off"
-              onClick={() => setEngine(toggleButton('ng_master'))}
-              on={() => engine().buttons.ng_master}
-            />
-          </WithLabel>
-          <WithLabel label="STARTER">
-            <Button
-              kind="on"
-              onClick={() => setEngine(toggleButton('ng_start'))}
-              on={() => engine().buttons.ng_start}
-            />
-          </WithLabel>
           <WithLabel label="SPEED">
-            <SevenSegment value={engine().ng_speed.toFixed(1)} digits={4} />
+            <SevenSegment
+              value={game().reactor().temp().toFixed(1)}
+              digits={4}
+            />
           </WithLabel>
         </Group>
         <Group direction="row" no_pad>
           <WithLabel label="SET POINT">
-            <SevenSegment value={engine().ng_set_point.toFixed(1)} digits={4} />
+            <SevenSegment
+              value={game().reactor().control_rods().toFixed(1)}
+              digits={4}
+            />
             <Encoder onChange={onSetPointChange} />
           </WithLabel>
         </Group>
